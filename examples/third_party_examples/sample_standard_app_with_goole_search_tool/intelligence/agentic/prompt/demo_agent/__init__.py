4b55bd1186a88fafb7154ce5223cb1c3c68611ad
Jerry Z H <jerry.zzw@antgroup.com>
2025-10-27 20:47:44 +0800

    feat: modify third_part_examples for sample_standard_app_with_goole_search_tool

--- examples/third_party_examples/sample_standard_app_with_goole_search_tool/intelligence/agentic/prompt/demo_agent/__init__.py
+++ examples/third_party_examples/sample_standard_app_with_goole_search_tool/intelligence/agentic/prompt/demo_agent/__init__.py
@@ -1,13 +1,7 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-# @Time    : 2024/3/13 14:29
+# @Time    : 2025/2/11 15:37
 # @Author  : wangchongshi
 # @Email   : wangchongshi.wcs@antgroup.com
 # @FileName: __init__.py
-
-from .github_tool import GitHubTool
-
-__all__ = [
-    'GitHubTool'
-]
