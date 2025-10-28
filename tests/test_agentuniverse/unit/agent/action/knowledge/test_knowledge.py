e0fd1ad4a8311b366082561acb74a1d68a9c9eb2
fanhaoxuee <1470600398@qq.com>
2025-10-29 00:21:08 +0800

    1029-1

--- tests/test_agentuniverse/unit/agent/action/knowledge/test_knowledge.py
+++ tests/test_agentuniverse/unit/agent/action/knowledge/test_knowledge.py
@@ -46,7 +46,9 @@ class KnowledgeTest(unittest.TestCase):
         store = self.knowledge.store
         query = Query(query_str='Which one is a cell phone?', similarity_top_k=1)
         res = store.query(query)
-        print(res)
+        self.assertIsNotNone(res)  
+        self.assertGreater(len(res), 0)  
+        self.assertIn('iPhone', res[0].text)
 
 
 if __name__ == '__main__':
